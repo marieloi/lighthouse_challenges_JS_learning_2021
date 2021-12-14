1fa71e961c0b133928e2a496f217f654c3948a40
Marilyne Eloi <96070597+marieloi@users.noreply.github.com>
2021-12-14 08:23:56 -0500

    Update challenge_17.js

--- challenge_17/challenge_17.js
+++ challenge_17/challenge_17.js
@@ -1,3 +1,11 @@
-const countActiveAstronauts = (roster) => {
-  return roster.length;
-}
+const switchSpecificToggle = (toggleList, specificToggle) => {
+
+  for (let i = 0; i < toggleList.length; i++) {
+    if (toggleList[i].name === specificToggle) {
+      toggleList[i].isOn = true;
+    }
+  }
+  
+  console.log(toggleList);
+  return toggleList;
+};
