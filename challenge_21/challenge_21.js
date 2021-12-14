b5091753967aac566c1ad95ae6628aeead81f4e2
Marilyne Eloi <96070597+marieloi@users.noreply.github.com>
2021-12-14 18:59:56 -0500

    Update challenge_21.js

--- challenge_21/challenge_21.js
+++ challenge_21/challenge_21.js
@@ -1,3 +1,12 @@
 const parseMissionSummary = (exchanges, missionData) => {
+  
+  let myObject = {};
+  myObject.transcript = [];
+  myObject.missionData = missionData;
 
-}
+  for(let i = 0; i < exchanges.length; i++){
+    myObject.transcript[i] = `${exchanges[i].origin}: ${exchanges[i].message}`;
+  }
+  
+  return myObject;
+};
