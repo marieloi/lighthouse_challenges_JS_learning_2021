55e1109b3e83edd79eb778f3c7c38456e88cf5c2
Marilyne Eloi <96070597+marieloi@users.noreply.github.com>
2021-12-14 14:48:06 -0500

    Update challenge_18.js

--- challenge_18/challenge_18.js
+++ challenge_18/challenge_18.js
@@ -3,7 +3,7 @@ const chooseLunchWinner = (listOfChoices) => {
   let map = {};
   let mostFrequentElement = listOfChoices[0];
   
-  for(let i = 0; i<listOfChoices.length; i++){
+  for(let i = 0; i < listOfChoices.length; i++){
       if (!map[listOfChoices[i]]) {
           map[listOfChoices[i]] = 1;
       } else {
