0a07114a72bdf8a10035a6d58e38d4842437818b
Marilyne Eloi <96070597+marieloi@users.noreply.github.com>
2021-12-14 16:53:48 -0500

    Update challenge_13.js

--- challenge_13/challenge_13.js
+++ challenge_13/challenge_13.js
@@ -1,14 +1,9 @@
 const checkAllGauges = (gaugeList) => {
   
-  let checkCondition;
-  
   for (let i = 0; i < gaugeList.length; i++) {
-    if (gaugeList[i].current <= gaugeList[i].min || gaugeList[i].current >= gaugeList[i].max) {
-      checkCondition = false;
-    } else {
-      checkCondition = true;
-    }
+    if (gaugeList[i].current < gaugeList[i].min || gaugeList[i].current > gaugeList[i].max)
+      return false;
   }
 
-  return checkCondition;
+  return true;
 };
