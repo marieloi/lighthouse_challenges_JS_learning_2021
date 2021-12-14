9b636b1718c92951cfb59f5d807cbf18a3ade02b
Marilyne Eloi <96070597+marieloi@users.noreply.github.com>
2021-12-14 15:35:24 -0500

    Update challenge_20.js

--- challenge_20/challenge_20.js
+++ challenge_20/challenge_20.js
@@ -1,4 +1,20 @@
 const confirmReentryPlans = (speed, missionData, checks) => {
+    
+  let doesEverythingMatch = false;
   
+  if(speed >= checks.minSpeed && speed <= checks.maxSpeed) {
+    for(let i = 0; i < Object.keys(checks.dataEntries).length; i++){
+      if (Object.keys(missionData)[i] === Object.keys(checks.dataEntries)[i]) {
+        if (Object.values(missionData)[i].length === Object.values(checks.dataEntries)[i]) {
+          doesEverythingMatch = true;
+        } else {
+          return false;
+        }
+      }
+    }
+  } else {
+    return false;
+  }
+  return doesEverythingMatch;
 
 }
