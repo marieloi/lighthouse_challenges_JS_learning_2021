0df8f789d3bad387ede9963a1d93574147f312ac
Marilyne Eloi <96070597+marieloi@users.noreply.github.com>
2021-12-14 16:45:37 -0500

    Update challenge_12.js

--- challenge_12/challenge_12.js
+++ challenge_12/challenge_12.js
@@ -2,7 +2,7 @@ const parseTranscripts = (messages) => {
   let myArray = [];
   
   messages.forEach(function (item) {
-    myArray.push(item.origin + ": " + item.message);
+    myArray.push(`${item.origin}: ${item.message}`);
   });
 
   return myArray;
