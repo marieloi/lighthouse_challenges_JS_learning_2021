2112eff794ba4948040e0d2a83b82f95a4e6e691
Marilyne Eloi <96070597+marieloi@users.noreply.github.com>
2021-12-14 16:44:09 -0500

    Update challenge_11.js

--- challenge_11/challenge_11.js
+++ challenge_11/challenge_11.js
@@ -1,12 +1,11 @@
 const bookFreePlatform = (platformList, missionDate) => {
-  
-for (let i = 0; i < platformList.length; i++) {
-  if (platformList[i].bookDate === undefined) {
-    platformList[i].bookDate  = missionDate;
-    break;
+
+  for (let i = 0; i < platformList.length; i++) {
+    if (platformList[i].bookDate === undefined) {
+      platformList[i].bookDate  = missionDate;
+      break;
+    }
   }
-}
 
   return platformList;
-
 };
